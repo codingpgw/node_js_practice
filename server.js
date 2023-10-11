4b4e75be0ef2873e0a39d6600d07db7557f1cf12
codingpgw <pgw20000@gmail.com>
2023-10-11 14:34:25 +0900

    mongodb v5로 수정

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended : true}));
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
@@ -11,11 +11,12 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 //app.use(express.static(__dirname + '/public'))
 
-var db;
+let db;
 
-MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.kwj0mwb.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
-    if (에러)return console.log(에러)
-    
+const url = 'mongodb+srv://admin:qwer1234@cluster0.kwj0mwb.mongodb.net/?retryWrites=true&w=majority';
+new MongoClient(url).connect().then((client) => {
+   
+    console.log('db연결성공')
     db = client.db('todoapp');
     
     app.listen(8080, function() {
@@ -23,68 +24,70 @@ MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.kwj0mwb.mongodb.net/?
     })
 
 
-    app.post('/add', function(요청, 응답){
+    app.post('/add', async (요청, 응답) => {
         응답.send('전송완료');
-        db.collection('counter').findOne({name: '게시물갯수'}, function(에러, 결과){
-            console.log(결과.totalPost);
-            var 총게시물갯수 = 결과.totalPost;
+        let add_result = await db.collection('counter').findOne({name: '게시물갯수'})
+            console.log(add_result.totalPost);
+            var 총게시물갯수 = add_result.totalPost;
 
-            db.collection('post').insertOne( {_id : 총게시물갯수 + 1, 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
-              console.log('저장완료');
-
-              db.collection('counter').updateOne({name : '게시물갯수'},{ $inc : {totalPost : 1}},function(에러,결과){
-                if(에러){return console.log(에러)}
-              })
+            await db.collection('post').insertOne({
+                _id: 총게시물갯수 + 1,
+                제목: 요청.body.title,
+                날짜: 요청.body.date
+            });
+            
+            await db.collection('counter').updateOne(
+                { name: '게시물갯수' },
+                { $inc: { totalPost: 1 } }
+            );
             //operator -> set, inc 등등 
             
-            });
-        });
     });
 
-    
-   
+}).catch((err)=>{
+    console.log(err)
 })
 
-app.delete('/delete', function(요청, 응답){
+app.delete('/delete', async (요청, 응답) => {
     console.log(요청.body);
     요청.body._id = parseInt(요청.body._id);
-    db.collection('post').deleteOne(요청.body,function(에러, 결과){
+    let del_result = await db.collection('post').deleteOne(요청.body)
         console.log('삭제완료');
         응답.status(200).send({message : '성공했습니다.'});
-    })
 })
 
-app.get('/list', function(요청, 응답){
+app.get('/list', async (요청, 응답) => {
 
-    db.collection('post').find().toArray(function(에러, 결과){
-        console.log(결과);
-        응답.render('list.ejs',{posts : 결과});
-    });
+    let result = await db.collection('post').find().toArray() 
+        console.log(result);
+        응답.render('list.ejs',{posts : result});
 
 })
-app.get('/detail/:id', function(요청, 응답){
-    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
-      응답.render('detail.ejs', {data : 결과} )
-    })
+
+app.get('/detail/:id', async (요청, 응답) => {
+    let detail_result = await db.collection('post').findOne({ _id : parseInt(요청.params.id) })
+
+      응답.render('detail.ejs', {data : detail_result} )
+
 });
 app.get('/write', function(요청, 응답){
     응답.render('write.ejs')
 })
 
-app.get('/edit/:id', function(요청, 응답){
+app.get('/edit/:id', async (요청, 응답) => {
     
-    db.collection('post').findOne({_id : parseInt(요청.params.id)},function(에러, 결과){
-        console.log(결과)
-        응답.render('edit.ejs', { post : 결과 })
-    })
+    let edit_result = await db.collection('post').findOne({_id : parseInt(요청.params.id) })
+        console.log(edit_result)
+        응답.render('edit.ejs', { post : edit_result })
+    
 })
 
-app.put('/edit',function(요청, 응답){
-    db.collection('post').updateOne({_id : parseInt(요청.body.id) },{$set : 
-        { 제목 : 요청.body.title, 날짜 : 요청.body.date}}, function(에러, 결과){
+app.put('/edit', async (요청, 응답) => {
+    let e_result = await db.collection('post').updateOne({_id : parseInt(요청.body.id) },{$set : 
+        { 제목 : 요청.body.title, 날짜 : 요청.body.date}})
             console.log('수정완료')
             응답.redirect('/list')
-    }) //폼에 담긴 제목 데이터, 날짜 데이터를 가지고 db.collection에 업데이트
+     //폼에 담긴 제목 데이터, 날짜 데이터를 가지고 db.collection에 업데이트
 })
 
 app.get('/pet', function(요청, 응답) { 
