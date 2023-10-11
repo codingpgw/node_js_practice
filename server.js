ecc22c29c741a236ba734d36c89748f363af72d9
codingpgw <pgw20000@gmail.com>
2023-10-11 11:21:33 +0900

    게시글 추가 삭제 수정기능 추가

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const bodyParser = require('body-parser');
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public'));
+//app.use(express.static(__dirname + '/public'))
 
 var db;
 
@@ -57,8 +62,29 @@ app.get('/list', function(요청, 응답){
     });
 
 })
-app.get('/detail/:id', function(){
-    응답.render('detal.ejs',{})
+app.get('/detail/:id', function(요청, 응답){
+    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+      응답.render('detail.ejs', {data : 결과} )
+    })
+});
+app.get('/write', function(요청, 응답){
+    응답.render('write.ejs')
+})
+
+app.get('/edit/:id', function(요청, 응답){
+    
+    db.collection('post').findOne({_id : parseInt(요청.params.id)},function(에러, 결과){
+        console.log(결과)
+        응답.render('edit.ejs', { post : 결과 })
+    })
+})
+
+app.put('/edit',function(요청, 응답){
+    db.collection('post').updateOne({_id : parseInt(요청.body.id) },{$set : 
+        { 제목 : 요청.body.title, 날짜 : 요청.body.date}}, function(에러, 결과){
+            console.log('수정완료')
+            응답.redirect('/list')
+    }) //폼에 담긴 제목 데이터, 날짜 데이터를 가지고 db.collection에 업데이트
 })
 
 app.get('/pet', function(요청, 응답) { 
@@ -73,9 +99,13 @@ app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html'); //이게  API (REST원칙에 맞춰 작성하자!)
 });
 
-app.get('/write', function(요청, 응답){
-    응답.sendFile(__dirname + '/write.html');
-});
+// app.get('/write', function(요청, 응답){
+//     응답.sendFile(__dirname + '/write.html');
+// });
+
+//app.get('/write', (요청, 응답 )*콜백함수 =>*function 대용 {
+//     응답.sendFile(__dirname + '/write.html');
+// })
 
 
 // app.post('/add', function(요청, 응답){
